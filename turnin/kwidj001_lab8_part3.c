9ac340f7389a473fcceeda531934626fdc82aaf4
WidjiWidji <karsten.widjanarko@gmail.com>
2021-02-14 21:47:49 -0800

    unable to provide demo video

--- turnin/kwidj001_lab8_part3.c
+++ turnin/kwidj001_lab8_part3.c
@@ -6,6 +6,7 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ Demo video: Hardware Issues unable to test on breadboard.
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
